f338b5a0cfa72096475262b5f00ace7eb04fb370
Júlio Abelha <julioabelha2311@gmail.com>
2022-04-18 00:17:47 -0300

    Commit 5, 6 e 7

---
+++ 5 - Utilizando variáveis float.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // Importando as funções 'printf' e 'scanf'
+
+int main() // Função principal
+{
+    int num1 = 5;
+    float num2 = 2;
+    float resultado = num1 / num2;
+
+    printf("%f", resultado);
+
+    return 0;
+}
